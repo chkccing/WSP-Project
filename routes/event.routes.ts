42f7ee1c19fbc8a112fd3500500708cf78de7ead
Clifford Lee <cliffordlkyhk@gmail.com>
2023-03-28 13:04:25 +0800

    add formidable

--- routes/event.routes.ts
+++ routes/event.routes.ts
@@ -2,9 +2,6 @@ import express, { Router } from 'express'
 import formidable from 'formidable'
 import { mkdirSync } from 'fs'
 import { join } from 'path'
-import { checkString, HttpError } from '../express'
-import { getSessionUser, hasLogin } from '../guards'
-
 export let eventRoutes = Router()
 
 let uploadDir = join('uploads', 'memo-images')
@@ -39,82 +36,3 @@ type Event = {
   Is_age18: Boolean
   Is_private: Boolean
 }
-
-let events: Event[] = [
-  { id: 1, host_id: 1, eventPicture?: 'null', title: 'testing', category: 'event', date: 2023-01-01, time: 2300-2400, details: '', hashtag: '#testing', cost: 0, Location: 'Hong Kong', participants: 2, FAQ: 'Internal Test only.', is_age18: false, is_private: false,},
-]
-let maxId = events.reduce((id, item) => Math.max(id, item.id), 0)
-
-EventRoutes.get('/events', (req, res) => {
-  res.json(events)
-})
-
-EventRoutes.post('/events', hasLogin, (req, res, next) => {
-  form.parse(req, (err, fields, files) => {
-    if (err) {
-      next(err)
-      return
-    }
-    try {
-      let host_id = getSessionUser(req).id
-      let imageMaybeArray = files.eventPicture
-      let eventPicture = Array.isArray(imageMaybeArray)
-        ? imageMaybeArray[0]
-        : imageMaybeArray
-      let title = checkString('title', fields.title)
-      let category = checkString('category', fields.category)
-      let details = checkString('details', fields.details)
-      let hashtag = checkString('hashtag', fields.hashtag)
-      let date = req.body.date
-      let time = req.body.time
-      let cost = req.body.cost
-      let location = checkString('location', fields.location)
-      let participants = req.body.is_private
-      let FAQ = checkString('title', fields.title)
-      let is_age18 = req.body.is_age18
-      let is_private = req.body.is_private
-      
-
-      maxId++
-      events.push({
-        id: maxId,
-        host_id,
-        eventPicture?: eventPicture?.newFilename,
-        title,
-        category,
-        date,
-        time,
-        details,
-        hashtag,
-        cost,
-        location,
-        participants,
-        FAQ,
-        is_age18,
-        is_private,
-      })
-      res.json({ id: maxId })
-    } catch (error) {
-      next(error)
-    }
-  })
-})
-
-EventRoutes.delete('/events/:id', hasLogin, (req, res) => {
-  let id = +req.params.id
-
-  let idx = events.findIndex(event => event.id === id)
-
-  if (idx === -1) {
-    res.json({ details: 'the event is already deleted' })
-    return
-  }
-
-  let event = events[idx]
-  if (event.host_id !== getSessionUser(req).id) {
-    throw new HttpError(403, "You are not allowed to delete other users' event")
-  }
-
-  events.splice(idx, 1)
-  res.json({ details: 'the event is just deleted' })
-})
