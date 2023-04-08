2e3e676647d84deb966c547d8c532d444bec2dce
Clifford Lee <cliffordlkyhk@gmail.com>
2023-04-08 12:58:31 +0800

    add manage and edit page

--- routes/event.routes.ts
+++ routes/event.routes.ts
@@ -101,6 +101,66 @@ eventRoutes.post("/createEvent", function (req: Request, res: Response) {
   })
 });
 
+//User Edit Event
+eventRoutes.post("/editEvent", function (req: Request, res: Response) {
+  form.parse(req, async (err, fields, files) => {
+    try {
+      // let eventPictureMaybeArray = files.eventPictures
+      // console.log("eventPictureMaybeArray:", files)
+      // let eventPicture = Array.isArray(eventPictureMaybeArray)
+      //   ? eventPictureMaybeArray[0]
+      //   : eventPictureMaybeArray.newFilename
+      let id = req.params.id
+      let event_id = req.query.eventId
+      let title = checkString('title', fields.title)
+      let category = checkString('category', fields.category)
+      let start_date = checkString('start_date', fields.start_date)
+      let end_date = checkString('end_date', fields.end_date)
+      let hashtag = checkString('hashtag', fields.hashtag)
+      let cost = Number(checkString('cost', fields.cost))
+      let location = checkString('location', fields.location)
+      let participants = Number(checkString('participants', fields.participants))
+      let faq = checkString('faq', fields.faq)
+      let is_age18 = checkBoolean('is_age18', fields.is_age18)
+      let is_private = checkBoolean('is_private', fields.is_private)
+      let result = await client.query(
+            /* sql */ `
+      select
+        event.id
+      from event
+      WHERE event.id = $1
+          `,
+        [id],
+      )
+      result = await client.query(
+            /* sql */ `
+      update event set
+      title = $1, 
+      category = $2, 
+      hashtag = $3, 
+      start_date = $4, 
+      end_date = $5, 
+      cost = $6, 
+      location = $7, 
+      participants = $8, 
+      faq = $9,
+      is_age18 = $10, 
+      is_private = $11
+      WHERE id = $12
+      returning id
+          `,
+        [title, category, hashtag, start_date, end_date, cost, location, participants, faq, is_age18, is_private, event_id],
+      )
+
+      res.json(result.rows[0].id);
+    } catch (error) {
+      console.log(error)
+      res.json({})
+    }
+  })
+});
+
+
 //Participants press the join button to join the event
 eventRoutes.post("/joinEvent", async (req: Request, res: Response) => {
   try {
@@ -168,7 +228,7 @@ eventRoutes.get("/allParticipants/:id", async (req, res, next) => {
     /* sql */`
     select * from users
     right join event_participant on users.id  = event_participant.user_id 
-    WHERE event_participant.event_id = $1
+    WHERE event_participant.event_id = $1 and event_participant.active = true
       `, [id],);
     let users = result.rows
     res.json({ users })
@@ -260,19 +320,18 @@ eventRoutes.post("/deleteParticipant", async (req: Request, res: Response) => {
     let result = await client.query(
             /* sql */ `
       select
-      event.id, event.host_id, event.active
-      from event
-      WHERE event.host_id = ${user_id} and event.active = true
+      * from event
+      inner join event_participant on event_participant.event_id = event.id
+      WHERE event.host_id = $1 and event.id = $2 and event_participant.active = true
           `,
-      [],
+      [user_id, event_id],
     )
     result = await client.query(
             /* sql */ `
-      update event set active = false 
-      WHERE id = $1
+      update event_participant set active = false 
       returning id
           `,
-      [event_id],
+      [],
     )
 
     let id = result.rows[0].id
@@ -281,6 +340,49 @@ eventRoutes.post("/deleteParticipant", async (req: Request, res: Response) => {
     res.json({})
   }
 });
+
+eventRoutes.get("/allCreateEvent", async (req, res, next) => {
+  try {
+    let user_id = getSessionUser(req).id
+    let result = await client.query(
+    /* sql */`
+    select id, host_id, eventPicture, title, end_date, active from event 
+    WHERE event.active = false and event.end_date >= NOW() and event.host_id = ${user_id}
+    ORDER BY start_date
+      `, [],);
+    let events = result.rows
+    res.json({ events })
+  } catch (error) {
+    next(error)
+  }
+}
+)
+
+eventRoutes.get("/allJoinedEvent", async (req, res, next) => {
+  try {
+    let user_id = getSessionUser(req).id
+
+    let result = await client.query(
+    /* sql */`
+    select event.id,
+    event.eventPicture, 
+    event.title, 
+    event.end_date,
+    event.active 
+    from event_participant 
+    inner join event on event_participant.event_id = event.id
+    WHERE event.active = false and event.end_date >= NOW() and event_participant.user_id = ${user_id}
+    ORDER BY start_date
+      `, [],);
+    let events = result.rows
+    res.json({ events })
+  } catch (error) {
+    next(error)
+  }
+}
+)
+
+// event.active = false and event.end_date >= NOW() and
 
 
 // eventRoutes.post("/eventSearch", async (req, res) => {
