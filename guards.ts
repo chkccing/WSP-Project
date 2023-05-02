b3f918eff759f302805e1594d6c02db41852bc05
Alan Yu <phoenixlieustrategy@gmail.com>
2023-05-02 10:56:50 +0800

    del some comments

--- guards.ts
+++ guards.ts
@@ -1,23 +1,23 @@
-import { NextFunction, Request, Response } from 'express'
-import { HttpError } from './express'
-import './session'
+import { NextFunction, Request, Response } from "express";
+import { HttpError } from "./express";
+import "./session";
 
 export function hasLogin(req: Request, res: Response, next: NextFunction) {
   if (req.session.user) {
-    next()
-    return
+    next();
+    return;
   }
-  res.status(401)
-  res.json({ error: 'This API is only for authenticated users' })
+  res.status(401);
+  res.json({ error: "This API is only for authenticated users" });
 }
 
 export function getSessionUser(req: Request) {
-  let user = req.session.user
-  //僅用於測試用，hard code 一個用戶登入session。
+  let user = req.session.user;
+  //belows are for test only. This is hard code a user for login session。
   // if (!user) {
   //   req.session.user = { id: 2, avatar: "dev", name: "dev" };
   //   user = req.session.user;
   // }
-  if (user) return user
-  throw new HttpError(401, 'This API is only for authenticated users')
+  if (user) return user;
+  throw new HttpError(401, "This API is only for authenticated users");
 }
